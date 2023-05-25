a93f6c2ed21d17733b8c5494a9be3cd1832afcfb
Andrey <orion128@mail.ru>
2023-05-25 20:28:58 +0300

    Change bonus

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int check = 300;
-        int popolnenie = 50;
-        int bonus = popolnenie / 100;
-        int itogchek2 = popolnenie + check;
-        int itogcheck1 = check + popolnenie + bonus;
+        int amaunt = 2300;
+        int bonus = amaunt / 100;
+        int total2 = amaunt + check;
+        int total1 = check + amaunt + bonus;
 
-        if (popolnenie >= 1000) {
-            System.out.println("Итоговая сумма на счету клиента составит:" + itogcheck1);
+        if (amaunt >= 1000) {
+            System.out.println("Итоговая сумма на счету клиента составит:" + total1);
             System.out.println("Бонус составит:" + bonus);
         } else {
-            System.out.println("Итоговая сумма на счету клиента составит:" + itogchek2);
+            System.out.println("Итоговая сумма на счету клиента составит:" + total2);
             System.out.println("Бонуса нет");
         }
+        
     }
 }
